d52558bb50e77c2b7e87a248e055b16f34590a81
Sergio Hermany Peter Cainzo <sergiocainzo@gmail.com>
2025-07-11 15:42:48 -0300

    Header and Footer
    Realizado a configuração do Header e do Footer

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,20 +1,22 @@
-import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  NgModule,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { Header } from './layout/header/header';
+import { Footer } from './layout/footer/footer';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App, Header, Footer],
+  imports: [BrowserModule, AppRoutingModule, CommonModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
